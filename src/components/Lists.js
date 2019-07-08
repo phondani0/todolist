db3adfd2f11e8c87b4b621f96701f1c376819ebc
Ankit <ankitphondani52@gmail.com>
2019-07-08 22:44:34 +0530

    Change state on click

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -3,46 +3,67 @@ import Todos from './Todos';
 import PropType from 'prop-types';
 
 class Lists extends Component {
-    state = {
-        lists: [{
-            id: 1,
-            title: 'Movies to watch',
-            todos: [{
+    constructor(props) {
+        super(props);
+        this.toggleOnClick = this.toggleOnClick.bind(this);
+
+        //State
+        this.state = {
+            lists: [{
                 id: 1,
-                title: 'Butter Fly Effect',
-                completed: false
+                showTodos: false,
+                title: 'Movies to watch',
+                todos: [{
+                    id: 1,
+                    title: 'Butter Fly Effect',
+                    completed: false
+                },
+                {
+                    id: 2,
+                    title: 'Forest Gump',
+                    completed: false
+                }]
             },
             {
                 id: 2,
-                title: 'Forest Gump',
-                completed: false
+                showTodos: false,
+                title: 'Places to visit',
+                todos: [{
+                    id: 1,
+                    title: 'Bangalore',
+                    completed: false
+                },
+                {
+                    id: 2,
+                    title: 'Mumbai',
+                    completed: false
+                }]
             }]
-        },
-        {
-            id: 2,
-            title: 'Places to visit',
-            todos: [{
-                id: 1,
-                title: 'Bangalore',
-                completed: false
-            },
-            {
-                id: 2,
-                title: 'Mumbai',
-                completed: false
-            }]
-        }]
+        }
+    }
+
+    toggleOnClick(id) {
+        let preState;
+        console.log(id);
+        preState = this.state.lists.map((list) => {
+            if (list.id === id) {
+                list.showTodos = !list.showTodos;
+            }
+            return list;
+        });
+        console.log(preState);
+        this.setState({ lists: preState });
     }
 
     render() {
         return this.state.lists.map((list) => {
             return (
-                <div key={list.id} className="card px-4 pt-3 pb-4 mb-3">
+                <div key={list.id} className="card px-3 py-2 mb-3">
                     <h3>{list.title}
-                        <i className="fa fa-sort-down"></i>
+                        <i className="fa fa-sort-down pl-2" onClick={() => this.toggleOnClick(list.id)}></i>
                     </h3>
                     <ul className="list-group list-unstyled">
-                        <Todos todos={list.todos} />
+                        {list.showTodos ? <Todos todos={list.todos} /> : null}
                     </ul>
                 </div>
             );
